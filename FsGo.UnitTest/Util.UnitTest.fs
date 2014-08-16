05fc8644412dab10b032aa1b8f631df52dcdf8eb
jk <john.kennerley@gmail.com>
2014-08-16 20:39:54 +0100

    initial commit.

---
+++ FsGo.UnitTest/Util.UnitTest.fs
@@ -0,0 +1,24 @@
+﻿namespace FsGo.UnitTest
+
+open Xunit
+open FsUnit.Xunit
+
+open Util
+open System.Diagnostics.CodeAnalysis
+
+[<ExcludeFromCodeCoverage>]
+type ``Util UnitTest``() = 
+
+    [<Fact>]
+    member this.``i2s 0 should -> "0"``() = 
+
+        // Assert
+        i2s 0 |> should equal "0" 
+        ()
+
+    [<Fact>]
+    member this.``i2s 1 should -> "!0"``() = 
+    
+        // Assert
+        i2s 1 |> should equal "!0" 
+        ()
---
+++ FsGo/Util.fs
@@ -0,0 +1,7 @@
+﻿module Util
+
+let i2s x = 
+    if x = 0 then 
+        "0"
+    else
+        "!0"
